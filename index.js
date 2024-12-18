4b6eae66abf7f2591481b57a6d36fbf6a5fd2c0b
TBag806 <boobsbigs@yahoo.com>
2024-12-18 12:04:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const Crypto = require('crypto')
 const path = require('path')
 const prefix = config.PREFIX
 
-const ownerNumber = ['237656520674']
+const ownerNumber = ['237697517505']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
