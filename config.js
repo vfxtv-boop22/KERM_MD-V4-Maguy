b779fbe77a95097cde1c0ce555d8b36aa1d5b0e1
vfxtv-boop22 <babydrago248@gmail.com>
2024-12-29 17:37:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "uExCQQKQ#Kbgt1wsRfDhxmjLYUSu3IowS1aRIdtqa9S4pbsNKc-U",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
